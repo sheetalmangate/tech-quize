2e2424750cbec661fbbcf4fc66bb731edd10b9f9
Sheetal Mangate <sheetal.mangate@gmail.com>
2024-12-15 16:22:24 -0800

    added component and e2e testing

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,23 +1,56 @@
-import React from 'react'
 import Quiz from '../../client/src/components/Quiz'
 
-describe('<Quiz />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Quiz />)
-  });
+describe('Quiz Component', () => {
 
-  it('should render the Start Quiz button', () => {
+  it('should display the Start Quiz button', () => {
 
+    cy.intercept('GET', '/api/questions/random', { statusCode: 200 }).as('getQuestion');
     cy.mount(<Quiz />);
     cy.get('button').should('have.text', 'Start Quiz');
     
   });
 
-  it('Start Quiz by click action on button', () => {
+  it('should start the Quiz and display question', () => {
+    cy.mount(<Quiz />)
+    cy.get('button').should('have.text', 'Start Quiz').click();
+    cy.get('.card').should('be.visible');
+    cy.get('h2').should('not.be.empty');
+  });
+
+
+  it('should able to choose answer', () => {
 
     cy.mount(<Quiz />);
     cy.get('button').should('have.text', 'Start Quiz').click();
+    cy.get('button').contains('1').click();
+    cy.get('.card').should('be.visible');
+    cy.get('h2').should('not.be.empty'); 
+  });
+
+  it('should answers questions and complete the quize', () => {
+
+    cy.mount(<Quiz />);
+    cy.get('button').should('have.text', 'Start Quiz').click();
+    
+    for(let i = 0; i<10; i++ ) {
+      cy.get('button').contains('1').click();
+    }
+
+    cy.get('.alert-success').should('be.visible').and('contain', 'Your score');
+    
+  });
+
+  it('should answers questions and render new quize', () => {
+
+    cy.mount(<Quiz />);
+    cy.get('button').should('have.text', 'Start Quiz').click();
+    
+    for(let i = 0; i<10; i++ ) {
+      cy.get('button').contains('1').click();
+    }
+    cy.get('button').contains('Take New Quiz').click();
+    cy.get('.card').should('be.visible');
+    cy.get('h2').should('not.be.empty');
     
   });
 
